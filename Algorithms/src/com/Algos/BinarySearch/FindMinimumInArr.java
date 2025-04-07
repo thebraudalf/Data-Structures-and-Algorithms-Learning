97327aeae7aa3e7334e2ec92cdfea21c1113a902
Nehal Jain <niihaljain2005@gmail.com>
2025-04-07 15:31:43 +0530

    fix typos

--- Algorithms/src/com/Algos/BinarySearch/FindMinimumInArr.java
+++ Algorithms/src/com/Algos/BinarySearch/FindMinimumInArr.java
@@ -1,6 +1,6 @@
 package com.Algos.BinarySearch;
 
-public class FindMinimumInArray {
+public class FindMinimumInArr {
     public static void main(String[] args) {
         int[] arr = {4, 5, 6, 7, 1, 2, 3};
         System.out.println(findMinimum(arr)); // 1
